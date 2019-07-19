cb3cc24559788321fe6ba0051ea6f9f4c16e3bb4
Yurii Trofimenko <tyaa@ukr.net>
2019-07-19 08:20:28 +0300

    fix image address (removed public word)

--- application/config/db-local.php
+++ application/config/db-local.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=abram-world',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=abram-world',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
